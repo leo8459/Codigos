786922011dad2c23e8ca6b704a23175de359e9ee
Leo <joseaguilar987654321@gmail.com>
2025-09-19 16:46:22 -0400

    Correcciones Captcha y ruta

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login'); // redirige al login
 });
 
 Route::get('/dashboard', function () {
